c9f19959bb5701a71f04b08873ca6a607b1fd5b8
TheOldFatherTW <weslie0705@gmail.com>
2026-09-17 21:33:39 +0800

    Point the public door at the current vault tunnel.

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-window.VAULT_ORIGIN = "https://euro-photographs-republican-assistant.trycloudflare.com";
-window.FAMIGATE_CONFIG = {
-  appId: "famibook",
-  viewKeyStorage: "famibook.viewKey",
-  productLabel: "FamiBook",
-};
+window.VAULT_ORIGIN = "https://winner-relationships-daisy-students.trycloudflare.com";
